be26c94c6e04faf4cc8ecb556bb2576af4955038
FIRST_NAME LAST_NAME <rb.firez@gmail.com>
2022-12-18 18:44:09 +0530

    initial

--- script.js
+++ script.js
@@ -11,10 +11,36 @@ function fillSvgPaths() {
     let path = paths[i];
 
     let pathLength = path.getTotalLength();
-    path.style.strokeDasharray = 5;
-    path.style.strokeDashoffset = 5;
+    path.style.strokeDasharray = pathLength;
+    path.style.strokeDashoffset = pathLength;
 
     let drawLength = pathLength * scrollPercentage;
     path.style.strokeDashoffset = pathLength - drawLength;
   }
 }
+
+// let paths = document.querySelectorAll("path");
+
+// fillSvgPaths();
+
+// document.addEventListener("scroll", fillSvgPaths);
+
+// function fillSvgPaths() {
+//   let scrollPercentage =
+//     (document.documentElement.scrollTop + document.body.scrollTop) /
+//     (document.documentElement.scrollHeight -
+//       document.documentElement.clientHeight);
+
+//   for (var i = 0; i < paths.length; i++) {
+//     let path = paths[i];
+
+//     let pathLength = path.getTotalLength();
+
+//     path.style.strokeDasharray = pathLength;
+//     path.style.strokeDashoffset = pathLength;
+
+//     let drawLength = pathLength * scrollPercentage;
+
+//     path.style.strokeDashoffset = pathLength - drawLength;
+//   }
+// }
